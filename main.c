7ae3b53ba2af2bf2f19ca8d4eaf10d02d7f843c6
laurent timothee <tlauren6@pc277.deptinfo-st.univ-fcomte.fr>
2018-10-08 13:32:09 +0200

    added main.c

--- main.c
+++ main.c
@@ -5,9 +5,5 @@
 
 int main() {
 	printf("hello");
-	struct list *self = malloc(sizeof(struct list));
-	list_create(self);
-	list_is_empty(self) ? printf("empty") : printf("pas empty");
-	list_destroy(self);
 	return 0;
 }
