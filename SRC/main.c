ce999557712c4a8104c3a538f9cd52584c29baf5
laurent timothee <timothee.laurent@yahoo.fr>
2018-10-12 09:04:54 +0200

    Created SRC/ and updated Makefile

--- SRC/main.c
+++ SRC/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "./INCLUDE/list.h"
-#include "./INCLUDE/graph.h"
+#include "../INCLUDE/list.h"
+#include "../INCLUDE/graph.h"
 
 int main() {
 	printf("hello");
