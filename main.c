f619c712daf79dcebb1dbe9ca48e739027ddf1b7
thibaultbertin <bertin.thibault.25@gmail.com>
2018-10-12 09:07:44 +0200

    debut main

--- main.c
+++ main.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "./INCLUDE/list.h"
-#include "./INCLUDE/graph.h"
+#include "../INCLUDE/list.h"
+#include "../INCLUDE/graph.h"
 
 int main() {
-	printf("hello");
+	int gameFinish = 0;
+	
+	while (!gameFinish) {
+		printf("---MENU---");
+		printf("1. Create graph");
+		printf("2. Load graph");
+		printf("3. Add node");
+		printf("4. Add edge");
+		printf("5. Remove node");
+		printf("6. Remove edge");
+		printf("7. View graph");
+		printf("8. Save graph");
+		printf("9. Quit");
+	}
+	
 	return 0;
 }
