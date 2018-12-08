5888ead9a609a26595c63bf52f35168fb1187d0b
Timothée <timothee.laurent@yahoo.fr>
2018-12-09 00:50:28 +0100

    refactor menus for actions 4 through 8

--- LIB/menu.c
+++ LIB/menu.c
@@ -206,12 +206,13 @@ void menu_add_edge(struct Graph *graph) {
 		printf("\n\t+---------------------------------------------+\n\t| /!\\ There are no nodes in the graph.        |\n\t| Please insert nodes before inserting edges. |\n\t+---------------------------------------------+\n\n");
 		return;
 	}
-	
+	printf("\n\t-----------------ADD EDGE-----------------\n\n");
 	do {
-		printf("\n\t-----------------ADD EDGE-----------------\n\n");
 		alreadyCreated = false;
+		
+		// INPUT THE TAIL NODE
 		do  {
-			printf("Write the number of the tail node : ");
+			printf("\nWrite the number of the tail node : ");
 			nodeTail = read_long();
 			
 			// Verifies that the tail node is in the graph
@@ -220,8 +221,9 @@ void menu_add_edge(struct Graph *graph) {
 			}
 		} while (graph->adjList[nodeTail-1] == NULL);
 		
+		// INPUT THE HEAD NODE
 		do  {
-			printf("Write the number of the head node : ");
+			printf("\nWrite the number of the head node : ");
 			
 			nodeHead = read_long();
 			
@@ -231,46 +233,58 @@ void menu_add_edge(struct Graph *graph) {
 			}
 		} while (graph->adjList[nodeHead-1] == NULL);
 		
-		if (graph->isDirected) {
-			// If the graph is directed we ask the user if he wants to add the symetric edge
-			long tmpChoice = 0;
-			do  {
-				printf("Do you want to add the corresponding symetric edge ?\n\n\t1. Yes\n\t2. No\n");
-				tmpChoice = read_long();
-			} while (tmpChoice != 0 && tmpChoice != 1);
-			symmetric = (tmpChoice == 1) ? true : false;
-		}
-		else {
-			symmetric = true;
-		}
+		// INPUT THE WEIGHT OF THE EDGE
+		do  {
+			if (weight < 0) {
+				printf("\n\t+----------------------------------+\n\t| /!\\ The weight can't be negative |\n\t+----------------------------------+\n");
+			}
+			printf("\nInput the weight of the edge : ");
+			weight = read_long();
+		} while (weight < 0);
 		
+		// Check if the edge doesn't already exist
 		struct Neighbour *curr = graph->adjList[nodeTail - 1];
 		while (curr != NULL) {
 			if (curr->neighbour == nodeHead) {
-				printf("This edge already exists in the graph. Please choose another value\n\n");
-				alreadyCreated = true;
+				printf("\n\t+--------------------------------------------+\n\t| /!\\ This edge already exists in the graph. |\n\t+--------------------------------------------+\n");
+				long choice = 0;
+				do {
+					printf("\nDo you want to try to add another edge?\n\t1. Yes\n\t2. Get back to the menu\n");
+					choice = read_long();
+				} while (choice < 1 || choice > 2);
+				if (choice == 1) {
+					alreadyCreated = true;
+				} else {
+					return;
+				}
 			}
 			curr = curr->nextNeighbour;
 		}
 		
-		if (symmetric) {
+		// Check if the symmetric doesn't already exist
+		if (graph->isDirected) {
 			struct Neighbour *curr = graph->adjList[nodeHead - 1];
 			while (curr != NULL) {
 				if (curr->neighbour == nodeTail) {
-					printf("This edge already exists in the graph. Please choose another value\n\n");
-					alreadyCreated = true;
+					symmetric = true;
 				}
 				curr = curr->nextNeighbour;
 			}
 		}
-	} while (alreadyCreated);
-	
-	do  {
-		printf("Write the weight of the edge : \n");
 		
-		weight = read_long();
-	} while (weight < 0);
-	if (add_edge(graph, nodeTail, nodeHead, weight, symmetric)) printf("The edge %ld: (%ld/%ld) was added to the graph with success!\n\n", nodeTail, nodeHead, weight);
+		// If the symmetric doesn't already exist
+		if (!symmetric && graph->isDirected) {
+			// We ask the user if he wants to add the symmetric
+			long tmpChoice = 0;
+			do  {
+				printf("\nDo you want to add the corresponding symmetric edge ?\n\n\t1. Yes\n\t2. No\n");
+				tmpChoice = read_long();
+			} while (tmpChoice < 1 || tmpChoice > 2);
+			symmetric = (tmpChoice == 1) ? true : false;
+		}
+	} while (alreadyCreated);
+	//printf("%d", add_edge(graph, nodeTail, nodeHead, weight, symmetric));
+	if (add_edge(graph, nodeTail, nodeHead, weight, symmetric) == 0) printf("\n\t+--------------------------------------------------------+\n\t| The edge %ld: (%ld/%ld) was added to the graph with success! |\n\t+--------------------------------------------------------+\n", nodeTail, nodeHead, weight);
 }
 
 /* Menu to remove a node from a graph
@@ -281,24 +295,27 @@ void menu_add_edge(struct Graph *graph) {
  */
 void menu_remove_node(struct Graph *graph) {
 
+	// If there are no nodes in the graph
 	if (get_node_number(graph) == 0) {
-		printf("/!\\ There are no nodes in the graph\n\n");
+		printf("\n\t+-----------------------------------+\n\t| /!\\ There are no nodes to remove. |\n\t+-----------------------------------+\n");
 		return;
-	} 
+	}
 	if (graph->adjList != NULL) {
+		printf("\n\t--------------REMOVE NODE-------------\n\n");
 		long node = 0;
 		do  {
-			printf("Write the number of the node you want to remove : \n");
+			printf("\nInput the number of the node you want to remove, 0 to cancel : ");
 		
 			node = read_long();
 		
 			// Verifies that the node is in the graph and that the node's number is correct
-			if (node > graph->nbMaxNodes || node < 1 || graph->adjList[node-1] == NULL) {
-				printf("This node isn't in the graph\n\n");
+			if (node > graph->nbMaxNodes || node < 0 || (node > 0 && graph->adjList[node-1] == NULL)) {
+					   
+				printf("\n\t+-----------------------------------+\n\t| /!\\ This node isn't in the graph. |\n\t+-----------------------------------+\n");
 			}
-		} while ((node > graph->nbMaxNodes) || (node < 1) || (graph->adjList[node-1] == NULL));
-	
-		if (remove_node(graph, node)) printf("The node %ld was removed from the graph with success!\n\n", node);
+		} while (node > graph->nbMaxNodes || node < 0 || (node > 0 && graph->adjList[node-1] == NULL));
+		if (node == 0) return;
+		if (remove_node(graph, node) == 0) printf("\n\t+-----------------------------------------------------+\n\t| The node %ld was removed from the graph with success! |\n\t+-----------------------------------------------------+\n\n", node);
 	} else {
 		menu_empty_graph(graph);
 	}
@@ -312,37 +329,36 @@ void menu_remove_node(struct Graph *graph) {
  */
 void menu_remove_edge(struct Graph *graph) {
 	if (get_node_number(graph) == 0) {
-		printf("/!\\ There are no edges in the graph\n\n");
+		printf("\n\t+-------------------------------------+\n\t| /!\\ There are no edges in the graph |\n\t+-------------------------------------+\n");
 		return;	
 	} 
 
 	if (graph->adjList != NULL) {
 		long nodeTail = 0;
 		long nodeHead = 0;
-		
+		printf("\n\t--------------REMOVE EDGE-------------\n\n");
 		do {
-			printf("Write the number of the tail node : \n");
+			printf("\nWrite the number of the tail node : ");
 			
 			nodeTail = read_long();
 			
 			// Verifies that the tail node is in the graph
 			if (graph->adjList[nodeTail-1] == NULL) {
-				printf("The tail node doesn't exist in the graph. Please choose another node\n\n");
+				printf("\n\t+-----------------------------------------------+\n\t| /!\\ The tail node doesn't exist in the graph. |\n\t| Please choose another node.                   |\n\t+-----------------------------------------------+\n");
 			}
 		} while (graph->adjList[nodeTail-1] == NULL);
 		
 		do {
-			printf("Write the number of the head node : \n");
+			printf("\nWrite the number of the head node : ");
 			
 			nodeHead = read_long();
 			
 			// Verifies that the head node is in the graph
 			if (graph->adjList[nodeHead-1] == NULL) {
-				printf("The head node doesn't exist in the graph. Please choose another node\n\n");
+				printf("\n\t+-----------------------------------------------+\n\t| /!\\ The head node doesn't exist in the graph. |\n\t| Please choose another node.                   |\n\t+-----------------------------------------------+\n");
 			}
 		} while (graph->adjList[nodeHead-1] == NULL);
-		
-		if (remove_edge(graph, nodeTail, nodeHead) == 0) printf("The edge was successfully removed from the graph!\n\n");
+		if (remove_edge(graph, nodeTail, nodeHead) == 0) printf("\n\t+---------------------------------------------------+\n\t| The edge was successfully removed from the graph! |\n\t+---------------------------------------------------+\n\n");
 	} else {
 		menu_empty_graph(graph);
 	}
@@ -356,7 +372,9 @@ void menu_remove_edge(struct Graph *graph) {
  */
 void menu_view_graph(struct Graph *graph) {
 	if (graph->adjList != NULL) {
+		printf("\n\t---------------VIEW GRAPH---------------\n\n");
 		view_graph(graph);
+		printf("\n\t----------------------------------------\n\n");
 	} else {
 		menu_empty_graph(graph);
 	}
@@ -370,13 +388,14 @@ void menu_view_graph(struct Graph *graph) {
  */
 void menu_save_graph(struct Graph *graph) {
 	if (graph->adjList != NULL) {
+		printf("\n\t---------------SAVE GRAPH---------------\n\n");
 		char *filename = malloc(1000 * sizeof(char));
 		do  {
-			printf("Write the name of the file : \n");
+			printf("Write the name of the file : ");
 		} while (read_string(filename, 1000) < 0);
 	
 		save_graph(graph, filename);
-		printf("The graph was successfully saved into %s. If you didn't specify an existing file, you will find your graph in SAVES/\n\n", filename);
+		printf("\n\t+-----------------------------------------+\n\t| The graph was successfully saved.       |\n\t| If you didn't specify an existing file, |\n\t| you will find your graph in SAVES/      |\n\t+-----------------------------------------+\n\n");
 		free(filename);
 	} else {
 		menu_empty_graph(graph);
